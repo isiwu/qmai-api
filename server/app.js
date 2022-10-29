e3653bcc8cdb95ce28af8ada1722e55dd9aceb09
Emmanuel <isiwuemma@gmail.com>
2022-10-29 08:18:11 +0100

    cors update

--- server/app.js
+++ server/app.js
@@ -119,14 +119,8 @@ app.use(express.static(join(__dirname, "../public")));
   }
 })();
 
-app.use(cors(
-  {
-    origin: "*",
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-  }
-));
-app.use("/api", cors(), indexRouter);
+app.use(cors());
+app.use("/api", indexRouter);
 
 //Error handlers
 app.use((req, res) => {
