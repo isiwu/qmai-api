c20020d2a4d099d0ad5b9b3a632522fb96da4646
Emmanuel <isiwuemma@gmail.com>
2022-11-14 16:02:28 +0100

    update

--- server/controllers/atpControllers.js
+++ server/controllers/atpControllers.js
@@ -292,6 +292,8 @@ getTrainees = (status) => async (req, res, next) => {
 
  res.locals.status = 200;
  res.locals.data = trainees;
+ console.log("atpId => ", atpId);
+ console.log("trainees => ", trainees);
  next();
 },
 addTraineeScore = async (req, res, next) => {
