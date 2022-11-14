99f4a4167b65423ba73deddbec884b88856421fd
Emmanuel <isiwuemma@gmail.com>
2022-11-14 21:16:46 +0100

    update on getATPTrainees

--- server/controllers/atpControllers.js
+++ server/controllers/atpControllers.js
@@ -281,10 +281,32 @@ getTrainees = (status) => async (req, res, next) => {
 
  let trainees = [];
  try {
-  if (status === "active") trainees = await Trainee.find({atpId, score: {$gt: 0}, cert: {isCertified: true, hasExpired: false}}).exec();
-  if (status === "inactive") trainees = await Trainee.find({atpId, score: {$gt: 0}, cert: {isCertified: true, hasExpired: true}}).exec();
-  if (status === "pending") trainees = await Trainee.find({atpId, score: {$gt: 0}, hasPayForCertificate: false}).exec();
-  if (status === "new") trainees = await Trainee.find({atpId, score: 0, hasPayForCertificate: false}).exec();
+  if (status === "active") trainees = await Trainee.find({
+    atpId, 
+    score: {$gt: 0}, 
+    "cert.isCertified": true, 
+    "cert.hasExpired": false
+  }).exec();
+
+  if (status === "inactive") trainees = await Trainee.find({
+    atpId, 
+    score: {$gt: 0}, 
+    "cert.isCertified": true, 
+    "cert.hasExpired": true
+  }).exec();
+
+  if (status === "pending") trainees = await Trainee.find({
+    atpId, 
+    score: {$gt: 0}, 
+    hasPayForCertificate: false, 
+    "cert.isCertified": false
+  }).exec();
+
+  if (status === "new") trainees = await Trainee.find({
+    atpId, 
+    score: 0, 
+    hasPayForCertificate: false
+  }).exec();
  } catch (error) {
   console.log(`Error in getting ATP trainees due to: ${error.message}`);
   return next(error);
