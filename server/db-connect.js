52ee7f313e0bd86d12417bc9f4abb41a0c60bddc
Emmanuel <isiwuemma@gmail.com>
2022-11-15 15:08:59 +0100

    update

--- server/db-connect.js
+++ server/db-connect.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   await new Promise((resolve, reject) => {
+    // let url;
+    // if (app.get("env") === "production") url = process.env.DB_URI;
+    // else url = "mongodb://localhost:27017/qsmai";
+
     mongoose.connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
